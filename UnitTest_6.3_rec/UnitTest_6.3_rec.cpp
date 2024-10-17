e25447722d0ddc28e9be0f0847f9fe2b8861a64d
vittkoo <dmytrovych.vv@gmail.com>
2024-10-17 15:35:07 +0300

    commit_1

---
+++ UnitTest_6.3_rec/UnitTest_6.3_rec.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ap_lab_6.3_rec/lab_6.3_rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestForArray
+{
+    TEST_CLASS(UnitTestForArray)
+    {
+    public:
+
+        TEST_METHOD(TestCreateArray)
+        {
+            const int n = 5;
+            int b[n];
+            int Low = -10, High = 10;
+
+            Create(b, n, Low, High, 0);
+
+            for (int i = 0; i < n; i++) {
+                Assert::IsTrue(b[i] >= Low && b[i] <= High);
+            }
+        }
+
+        TEST_METHOD(TestMinOdd)
+        {
+            // 1. масив з непарними елементами
+            const int n1 = 5;
+            int a1[n1] = { 12, 7, 5, 20, 9 };
+            int expectedMinOdd1 = 5;
+            int minOdd;
+
+            int actualMinOdd1 = MinOdd(a1, n1, minOdd, IFirst(a1, n1, minOdd, 0));
+            Assert::AreEqual(expectedMinOdd1, actualMinOdd1);
+
+            // 2. масив з одним непарним елементом
+            const int n3 = 4;
+            int a3[n3] = { 12, 14, 9, 22 };
+            int expectedMinOdd3 = 9;
+
+            int actualMinOdd3 = MinOdd(a3, n3, minOdd, IFirst(a3, n3, minOdd, 0));
+            Assert::AreEqual(expectedMinOdd3, actualMinOdd3);
+        }
+    };
+}
---
+++ ap_lab_6.3_rec/lab_6.3_rec.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High, int i) {
+    a[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(a, size, Low, High, i + 1);
+}
+
+void Print(int* a, const int size, int i) {
+    cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+
+int IFirst(int* a, const int size, int& minOdd, int i) {
+    if (a[i] % 2 != 0) {
+        minOdd = a[i];
+        return i;
+    }
+
+    if (i < size - 1)
+        return IFirst(a, size, minOdd, i + 1);
+    else
+        return -1;
+}
+
+int MinOdd(int* a, const int size, int minOdd, int i) {
+    if (a[i] < minOdd && a[i] % 2 != 0)
+        minOdd = a[i];
+
+    if (i < size - 1)
+        return MinOdd(a, size, minOdd, i + 1);
+    else
+        return minOdd;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    const int n = 10;
+    int a[n];
+
+    int Low = -10;
+    int High = 10;
+
+    Create(a, n, Low, High, 0);
+    Print(a, n, 0);
+
+    int minOdd;
+    int imin = IFirst(a, n, minOdd, 0);
+
+    if (imin == -1)
+        cerr << "There`s no odd elements!" << endl;
+    else
+        cout << "minOdd = " << MinOdd(a, n, minOdd, imin + 1) << endl;
+
+    return 0;
+}
